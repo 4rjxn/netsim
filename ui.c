694e2707a8e54dfdd3a4c57fd858a1a61c85b106
4rjxn <arjunaskvl@gmail.com>
2026-09-20 22:42:48 +0530

    added a seperate ui file to handle the IO
    the file currently have a readLine, readInt and readName impelemented.
    the next function to implement is readDeviceType.

---
+++ ui.c
@@ -0,0 +1,73 @@
+#include "ui.h"
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void trim(char *str) {
+  char *start = str;
+  while (isspace((unsigned char)*start) && *start != '\0') {
+    start++;
+  }
+  if (*start == '\0') {
+    *str = '\0';
+    return;
+  }
+  char *end = start + strlen(start) - 1;
+  while (end > start && isspace((unsigned char)*end)) {
+    end--;
+  }
+  *(end + 1) = '\0';
+
+  memmove(str, start, strlen(start) + 1);
+}
+
+InputStatus readLine(char *buf, size_t size) {
+  char *res = fgets(buf, size, stdin);
+  if (res == NULL) {
+    return INPUT_EOF;
+  }
+  char *c;
+  if ((c = strchr(buf, '\n')) != NULL) {
+    *c = '\0';
+  } else {
+    int ch;
+    while ((ch = getc(stdin)) != EOF && ch != '\n') {
+    }
+  }
+  return INPUT_OK;
+}
+
+InputStatus readInt(int *read_value) {
+  char buf[20];
+  InputStatus st = readLine(buf, sizeof(buf));
+  if (st != INPUT_OK) {
+    return st;
+  }
+  char *end;
+  errno = 0;
+  long res = strtol(buf, &end, 10);
+  if (end == buf || *end != '\0' || errno == ERANGE ||
+      !(res <= INT_MAX && res >= INT_MIN)) {
+    return INPUT_INVALID;
+  }
+  *read_value = res;
+  return INPUT_OK;
+}
+
+InputStatus readName(char *read_value, size_t size) {
+  char buf[64];
+  InputStatus st = readLine(buf, sizeof(buf));
+  if (st != INPUT_OK) {
+    return st;
+  }
+  trim(buf);
+  if (*buf == '\0' || strlen(buf) >= size) {
+    return INPUT_INVALID;
+  }
+  strcpy(read_value, buf);
+  return INPUT_OK;
+}
---
+++ ui.h
@@ -0,0 +1,8 @@
+#ifndef STATUS_H
+#define STATUS_H
+#pragma once
+
+enum InputStatus { INPUT_OK, INPUT_INVALID, INPUT_EOF };
+typedef enum InputStatus InputStatus;
+
+#endif
